125ab398bd20b6dbca9e620b3eb84490e87d3d8b
Asuna <SpriteOvO@gmail.com>
2024-06-29 14:17:40 +0800

    [Telegram] Retry with HTTP multipart if sending media with URL failed

--- src/notify/telegram/request.rs
+++ src/notify/telegram/request.rs
@@ -1,12 +1,17 @@
-use std::{borrow::Cow, ops::Range};
+use std::{borrow::Cow, io::Cursor, ops::Range};
 
 use anyhow::anyhow;
+use bytes::Bytes;
+use futures::{stream::TryStreamExt, StreamExt};
+use image::{imageops::FilterType as ImageFilterType, GenericImageView};
 use itertools::Itertools;
+use reqwest::multipart::{Form, Part};
 use serde::{
     de::{DeserializeOwned, IgnoredAny},
     Deserialize,
 };
 use serde_json::{self as json, json};
+use spdlog::prelude::*;
 
 use super::ConfigChat;
 
@@ -20,14 +25,64 @@ impl<'a> Request<'a> {
     }
 
     async fn send_request<T: DeserializeOwned>(
-        self,
+        &self,
         method: &str,
-        body: json::Value,
+        body: &json::Value,
+    ) -> anyhow::Result<Response<T>> {
+        Self::send_request_with_files(self, method, body, None as Option<[FileUrl; 0]>).await
+    }
+
+    async fn send_request_with_files<T: DeserializeOwned>(
+        &self,
+        method: &str,
+        body: &json::Value,
+        // If `file_urls` specified, the media fields in body should be replaced with
+        // "attach://{index}"
+        file_urls: Option<impl IntoIterator<Item = FileUrl<'_>>>,
     ) -> anyhow::Result<Response<T>> {
         let url = format!("https://api.telegram.org/bot{}/{}", self.token, method);
-        let resp = reqwest::Client::new()
-            .post(url)
-            .json(&body)
+
+        let mut client = reqwest::Client::new().post(url);
+
+        if let Some(file_urls) = file_urls {
+            let form = form_append_json(Form::new(), body.as_object().unwrap());
+            let form = futures::stream::iter(file_urls)
+                .enumerate()
+                // I don't know why `try_fold` in `futures` takes a iterator of `Result`..
+                .map(anyhow::Ok)
+                .try_fold(form, |form, (i, file_url)| async move {
+                    trace!("downloading media from url '{}'", file_url.url);
+
+                    let file = reqwest::get(file_url.url).await.map_err(|err| {
+                        anyhow!("failed to download file: {err} from url '{}'", file_url.url)
+                    })?;
+
+                    let status = file.status();
+                    anyhow::ensure!(
+                        status.is_success(),
+                        "response of downloading file is not success: {status} from url '{}'",
+                        file_url.url
+                    );
+
+                    let bytes = file.bytes().await.map_err(|err| {
+                        let rustfmt_bug = "failed to obtain bytes for downloading file";
+                        anyhow!(
+                            "{rustfmt_bug}: {err}, status: {status} from url '{}'",
+                            file_url.url
+                        )
+                    })?;
+
+                    // TODO: Replace failed image with a fallback image
+
+                    Ok(form.part(i.to_string(), media_into_part(i, bytes, file_url.is_photo)?))
+                })
+                .await?;
+            client = client.multipart(form);
+        } else {
+            client = client.json(&body);
+        }
+
+        let resp = client
             .send()
             .await
             .map_err(|err| anyhow!("failed to send request: {err}"))?;
@@ -100,6 +155,28 @@ impl<'a> Request<'a> {
             chat,
             message_id,
             text: None,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct FileUrl<'a> {
+    url: &'a str,
+    is_photo: bool,
+}
+
+impl<'a> FileUrl<'a> {
+    fn new_photo(url: &'a str) -> Self {
+        Self {
+            url,
+            is_photo: true,
+        }
+    }
+
+    fn new_video(url: &'a str) -> Self {
+        Self {
+            url,
+            is_photo: false,
         }
     }
 }
@@ -221,7 +298,7 @@ impl<'a> SendMessage<'a> {
         if let Some(markup) = self.markup {
             body["reply_markup"] = markup.into_json();
         }
-        self.base.send_request("sendMessage", body).await
+        self.base.send_request("sendMessage", &body).await
     }
 }
 
@@ -288,14 +365,14 @@ impl<'a> SendMedia<'a> {
                 "disable_notification": self.disable_notification
             }
         );
-        let method = match self.media {
+        let (method, url) = match self.media {
             Media::Photo(url) => {
                 body["photo"] = url.into();
-                "sendPhoto"
+                ("sendPhoto", url)
             }
             Media::Video(url) => {
                 body["video"] = url.into();
-                "sendVideo"
+                ("sendVideo", url)
             }
         };
         if let Some(text) = self.text {
@@ -307,7 +384,27 @@ impl<'a> SendMedia<'a> {
         if let Some(markup) = self.markup {
             body["reply_markup"] = markup.into_json();
         }
-        self.base.send_request(method, body).await
+
+        let mut resp = self.base.send_request(method, &body).await?;
+        if is_media_failure(&resp) {
+            warn!("failed to send media with URL, retrying with HTTP multipart. url '{url}', description '{}'", resp.description.as_deref().unwrap_or("*no description*"));
+
+            let file_url = match self.media {
+                Media::Photo(_) => {
+                    body["photo"] = format_attach_url(0).into();
+                    FileUrl::new_photo(url)
+                }
+                Media::Video(_) => {
+                    body["video"] = format_attach_url(0).into();
+                    FileUrl::new_video(url)
+                }
+            };
+            resp = self
+                .base
+                .send_request_with_files(method, &body, Some([file_url]))
+                .await?;
+        }
+        Ok(resp)
     }
 }
 
@@ -368,7 +465,7 @@ impl<'a> SendMediaGroup<'a> {
 
         let mut media = self
             .medias
-            .into_iter()
+            .iter()
             .map(|media| match media {
                 Media::Photo(url) => {
                     json!({
@@ -391,7 +488,7 @@ impl<'a> SendMediaGroup<'a> {
             first_media.insert("caption_entities".into(), entities);
         }
 
-        let body = json!(
+        let mut body = json!(
             {
                 "chat_id": self.chat.to_json(),
                 "message_thread_id": self.thread_id,
@@ -399,7 +496,32 @@ impl<'a> SendMediaGroup<'a> {
                 "disable_notification": self.disable_notification
             }
         );
-        self.base.send_request("sendMediaGroup", body).await
+
+        let mut resp = self.base.send_request("sendMediaGroup", &body).await?;
+        if is_media_failure(&resp) {
+            let file_urls = body.as_object_mut().unwrap()["media"]
+                .as_array_mut()
+                .unwrap()
+                .iter_mut()
+                .zip(self.medias)
+                .enumerate()
+                .map(|(i, (media, kind))| {
+                    media["media"] = format_attach_url(i).into();
+                    match kind {
+                        Media::Photo(url) => FileUrl::new_photo(url),
+                        Media::Video(url) => FileUrl::new_video(url),
+                    }
+                })
+                .collect::<Vec<_>>();
+
+            warn!("failed to send media group with URLs, retrying with HTTP multipart. urls '{file_urls:?}', description '{}'", resp.description.as_deref().unwrap_or("*no description*"));
+
+            resp = self
+                .base
+                .send_request_with_files("sendMediaGroup", &body, Some(file_urls))
+                .await?;
+        }
+        Ok(resp)
     }
 }
 
@@ -431,7 +553,7 @@ impl<'a> EditMessageCaption<'a> {
             body.insert("caption".into(), text);
             body.insert("caption_entities".into(), entities);
         }
-        self.base.send_request("editMessageCaption", body).await
+        self.base.send_request("editMessageCaption", &body).await
     }
 }
 
@@ -532,4 +654,82 @@ impl<R> Response<R> {
 #[derive(Deserialize)]
 pub struct ResultMessage {
     pub message_id: i64,
+}
+
+fn is_media_failure<T>(resp: &Response<T>) -> bool {
+    if resp.ok || resp.description.is_none() {
+        return false;
+    }
+
+    let description = resp.description.as_deref().unwrap();
+
+    description.contains("WEBPAGE_") || // https://core.telegram.org/method/messages.sendMedia
+    description.contains("failed to get HTTP URL content") ||
+    description.contains("wrong file identifier/HTTP URL specified")
+}
+
+fn format_attach_url(index: usize) -> String {
+    format!("attach://{index}")
+}
+
+fn form_append_json(form: Form, obj: &json::Map<String, json::Value>) -> Form {
+    obj.iter().fold(form, |form, (key, value)| {
+        let value = match value {
+            json::Value::Null => None,
+            json::Value::Bool(value) => Some(value.to_string()),
+            json::Value::Number(value) => Some(value.to_string()),
+            json::Value::String(value) => Some(value.clone()),
+            json::Value::Array(value) => Some(json::to_string(value).unwrap()),
+            json::Value::Object(value) => Some(json::to_string(value).unwrap()),
+        };
+        if let Some(value) = value {
+            form.text(key.clone(), value)
+        } else {
+            form
+        }
+    })
+}
+
+fn media_into_part(i: usize, bytes: Bytes, is_photo: bool) -> anyhow::Result<Part> {
+    let part = if is_photo {
+        let image_reader = image::io::Reader::new(Cursor::new(&bytes))
+            .with_guessed_format()
+            .map_err(|err| anyhow!("failed to guess format for downloaded image: {err}"))?;
+
+        let format = image_reader.format();
+        let mut image = image_reader
+            .decode()
+            .map_err(|err| anyhow!("failed to decode downloaded image: {err}"))?;
+        let (width, height) = image.dimensions();
+
+        // Based on my testing, the // actual limit is <=10001 :)
+        const LIMIT: u32 = 10000;
+
+        let total = width + height;
+        if total > LIMIT {
+            // Scaledown
+            let ratio = (width as f64 / total as f64, height as f64 / total as f64);
+            let new = (
+                (LIMIT as f64 * ratio.0).floor() as u32,
+                (LIMIT as f64 * ratio.1).floor() as u32,
+            );
+            warn!(
+                "photo #{i} dimensions {width},{height} exceeds the limit, scale down to {},{}",
+                new.0, new.1
+            );
+
+            image = image.resize(new.0, new.1, ImageFilterType::Lanczos3);
+
+            let mut bytes: Vec<u8> = Vec::new();
+            image.write_to(&mut Cursor::new(&mut bytes), format.unwrap())?;
+            Part::stream(bytes)
+        } else {
+            Part::stream(bytes)
+        }
+    } else {
+        Part::stream(bytes)
+    }
+    .file_name("");
+
+    Ok(part)
 }
