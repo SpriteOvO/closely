695c8fe3e44b5194f25867ab9ea201f3f8721e10
Asuna <SpriteOvO@gmail.com>
2025-01-31 01:08:15 +0800

    Embed detailed version info in `--version` cli command

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,18 @@
 use std::path::PathBuf;
 
+use shadow_rs::{formatcp, shadow};
+
+shadow!(build);
+use build::*;
+
+const VER: &str = formatcp!(
+    "{}, {BRANCH} ({SHORT_COMMIT}{}), {BUILD_TIME}, {BUILD_RUST_CHANNEL}",
+    env!("CARGO_PKG_VERSION"),
+    if !build::GIT_CLEAN { ", dirty" } else { "" }
+);
+
 #[derive(clap::Parser, Debug)]
-#[command(version)]
+#[command(version, long_version = VER)]
 pub struct Args {
     #[arg(long)]
     pub config: PathBuf,
