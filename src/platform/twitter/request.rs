5501168eaed6c282311ef0debbc88b3311cf5c95
Asuna <SpriteOvO@gmail.com>
2025-08-10 00:19:33 +0800

    [Twitter] Update API endpoints and structures

--- src/platform/twitter/request.rs
+++ src/platform/twitter/request.rs
@@ -45,27 +45,29 @@ impl TwitterRequester {
 
         let variables = json!({
             "screen_name": screen_name,
-            "withSafetyModeUserFields": true
+            "withGrokTranslatedBio": false
         });
         let features = json!({
             "hidden_profile_subscriptions_enabled": true,
+            "payments_enabled": false,
+            "rweb_xchat_enabled": false,
+            "profile_label_improvements_pcf_label_in_post_enabled": true,
             "rweb_tipjar_consumption_enabled": true,
-            "responsive_web_graphql_exclude_directive_enabled": true,
             "verified_phone_label_enabled": false,
             "subscriptions_verification_info_is_identity_verified_enabled": true,
             "subscriptions_verification_info_verified_since_enabled": true,
             "highlights_tweets_tab_ui_enabled": true,
             "responsive_web_twitter_article_notes_tab_enabled": true,
-            "subscriptions_feature_can_gift_premium": false,
+            "subscriptions_feature_can_gift_premium": true,
             "creator_subscriptions_tweet_preview_api_enabled": true,
             "responsive_web_graphql_skip_user_profile_image_extensions_enabled": false,
             "responsive_web_graphql_timeline_navigation_enabled": true
         });
         let field_toggles = json!({
-            "withAuxiliaryUserLabels": false
+            "withAuxiliaryUserLabels": true
         });
         let mut url =
-            Url::from_str("https://x.com/i/api/graphql/xmU6X_CKVnQ5lSrCbAmJsg/UserByScreenName")?;
+            Url::from_str("https://x.com/i/api/graphql/gEyDv8Fmv2BVTYIAf32nbA/UserByScreenName")?;
         {
             let mut query = url.query_pairs_mut();
             query.append_pair("variables", &json::to_string(&variables)?);
@@ -83,43 +85,53 @@ impl TwitterRequester {
 
         let variables = json!({
             "userId": user_id,
-            "count": 20,
+            "count": 40,
             "includePromotedContent": true,
             "withQuickPromoteEligibilityTweetFields": true,
             "withVoice": true,
-            "withV2Timeline": true
         });
         let features = json!({
+            "rweb_video_screen_enabled": false,
+            "payments_enabled": false,
+            "rweb_xchat_enabled": false,
+            "profile_label_improvements_pcf_label_in_post_enabled": true,
             "rweb_tipjar_consumption_enabled": true,
-            "responsive_web_graphql_exclude_directive_enabled": true,
             "verified_phone_label_enabled": false,
             "creator_subscriptions_tweet_preview_api_enabled": true,
             "responsive_web_graphql_timeline_navigation_enabled": true,
             "responsive_web_graphql_skip_user_profile_image_extensions_enabled": false,
+            "premium_content_api_read_enabled": false,
             "communities_web_enable_tweet_community_results_fetch": true,
             "c9s_tweet_anatomy_moderator_badge_enabled": true,
+            "responsive_web_grok_analyze_button_fetch_trends_enabled": false,
+            "responsive_web_grok_analyze_post_followups_enabled": true,
+            "responsive_web_jetfuel_frame": true,
+            "responsive_web_grok_share_attachment_enabled": true,
             "articles_preview_enabled": true,
-            "tweetypie_unmention_optimization_enabled": true,
             "responsive_web_edit_tweet_api_enabled": true,
             "graphql_is_translatable_rweb_tweet_is_translatable_enabled": true,
             "view_counts_everywhere_api_enabled": true,
             "longform_notetweets_consumption_enabled": true,
             "responsive_web_twitter_article_tweet_consumption_enabled": true,
             "tweet_awards_web_tipping_enabled": false,
+            "responsive_web_grok_show_grok_translated_post": false,
+            "responsive_web_grok_analysis_button_from_backend": true,
             "creator_subscriptions_quote_tweet_preview_enabled": false,
             "freedom_of_speech_not_reach_fetch_enabled": true,
             "standardized_nudges_misinfo": true,
             "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": true,
-            "rweb_video_timestamps_enabled": true,
             "longform_notetweets_rich_text_read_enabled": true,
             "longform_notetweets_inline_media_enabled": true,
+            "responsive_web_grok_image_annotation_enabled": true,
+            "responsive_web_grok_imagine_annotation_enabled": true,
+            "responsive_web_grok_community_note_auto_translation_is_enabled": false,
             "responsive_web_enhance_cards_enabled": false
         });
         let field_toggles = json!({
             "withArticlePlainText": false
         });
         let mut url =
-            Url::from_str("https://x.com/i/api/graphql/V7H0Ap3_Hh2FyS75OCDO3Q/UserTweets")?;
+            Url::from_str("https://x.com/i/api/graphql/BqvqNsqColIQbpX1_NmEwg/UserTweets")?;
         {
             let mut query = url.query_pairs_mut();
             query.append_pair("variables", &json::to_string(&variables)?);
